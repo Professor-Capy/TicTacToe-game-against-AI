a2d563271350cada4b0654da82429ba6d0485ccc
Professor-Capy <tinnacherjosef42@gmail.com>
2026-05-30 19:09:40 +0800

    moved result and player functions to logic.py

--- README.txt
+++ README.txt
@@ -1,8 +1,5 @@
 state is a 2d list, or array
 
-If a problem is shown, saying a problem abt
-the structs_2d import in minimax.py, dont mind it
-
 X ALWAYS plays first, some code is based on that assumption
 
 i guess dataStructs also has a Queue, but its not rly needed here
--- minimax.py
+++ minimax.py
@@ -1,21 +1,2 @@
 from dataStructs import Stack
 
-# used for determining the player
-def player(state: list):
-    x: int = 0
-    o: int = 0
-    for row in state: # for each row of the board in the current state
-        for box in row: # and for each box in that row
-            # it isnt that hard cmon now
-            match box:
-                case 'X':
-                    x += 1
-                case 'O':
-                    o += 1
-
-    # X always plays first, so if x isnt greater than o,
-    # it is equal to o, removing the need for an else statement
-    if x > o:
-        return 'O'
-    return 'X'
-
